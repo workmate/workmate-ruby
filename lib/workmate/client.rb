c81808b171d6a1f85d75af4b5cfe0f6a787a2992
Iskander Haziev <gvalmon@gmail.com>
2011-11-22 01:46:30 +0400

    fix api url

--- lib/workmate/client.rb
+++ lib/workmate/client.rb
@@ -2,16 +2,16 @@ require 'json'
 require 'oauth2'
 module Workmate
   class Client
-    BASE_URL = 'https://workmateapp.com'
-    API_URL = "#{BASE_URL}/api"
+    BASE_URL = 'http://workmateapp.com'
     
     def initialize(client_id, client_secret, token = nil, url = BASE_URL)
+      @base_url = url
       @client = OAuth2::Client.new(client_id, client_secret, :site => url)
       @token = OAuth2::AccessToken.from_hash(@client, access_token: token)
     end
     
     def call(uri)
-      response = @token.get("#{API_URL}#{uri}.json", parse: :json)
+      response = @token.get("#{api_url}#{uri}.json", parse: :json)
       response.parsed
     end
     
@@ -21,6 +21,11 @@ module Workmate
     
     def user(user_id)
       call('/users/' + user_id)
+    end
+    
+    protected
+    def api_url
+      "#{@base_url}/api"
     end
   end
 end
